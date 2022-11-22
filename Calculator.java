7ba1ac62b1a2f32590048c546ac23ce07bc91818
Elucian Moise <elucian.moise@gmail.com>
2022-11-22 23:41:04 +0000

    refectory

--- Calculator.java
+++ Calculator.java
@@ -1,25 +1,10 @@
 public class Calculator {
   
-    interface IntegerMath {
+    public interface IntegerMath {
         int operation(int a, int b);   
     }
   
     public int operateBinary(int a, int b, IntegerMath op) {
         return op.operation(a, b);
-    }
- 
-    public static void main(String... args) {
-    
-        Calculator myApp = new Calculator();
-        IntegerMath addition = (a, b) -> a + b;
-        IntegerMath subtraction = (a, b) -> a - b;
-
-        // call using lamda expression as argument
-        System.out.println("40 + 2 = " +
-            myApp.operateBinary(40, 2, addition));
-
-        // call using lamda expression as argument    
-        System.out.println("20 - 10 = " +
-            myApp.operateBinary(20, 10, subtraction));    
     }
 }
--- Main.java
+++ Main.java
@@ -1,47 +1,90 @@
 import static java.lang.System.out;
 
 class Main {
- public static void print_menu() {
-    clear(); 
+  private static Calculator.IntegerMath addition;
+  private static Calculator.IntegerMath subtraction;
+  private static Calculator myApp;
+  public static void print_menu() {
+    clear();
     out.println("-".repeat(30));
-    out.println("Account Management!");
+    out.println("Options!");
     out.println("-".repeat(30));
-    out.println("0 - Clear console"); 
-    out.println("1 - Hello World");         
-    out.println("9 - Exit"); 
-    out.println("-".repeat(30));    
+    out.println("0 - Clear console");
+    out.println("1 - Help");
+    out.println("2 - Compute");
+    out.println("9 - Exit");
+    out.println("-".repeat(30));
   }
-  
+
   public static void main(String[] args) {
-    int option  = 0;
+    int option = 0; // no option
+    // prepare caclulator & operators
+    myApp = new Calculator();
+
+    // two lambda expressions
+    addition = (a, b) -> a + b;
+    subtraction = (a, b) -> a - b;
+
+    // execution loop
     do {
       switch (option) {
         case 0:
-          print_menu(); break;
-        case 1:
-          out.println("Hello World");
+          print_menu();
           break;
-        case 9:break;          
+        case 1:
+          out.println("""
+              This is a calculator,
+              Work in progress.
+              Enter a option (0 to 9) 
+              for a quick test.""");
+          break;
+        case 2:
+          compute();
+          break;
+        case 9:
+          break;
         default:
           out.format("option %d " +
-                     "not implemented.%n",option);
+              "not implemented.%n", option);
           break;
       }
-      option = read_option();       
-    } while (option < 9);      
+      option = read_option();
+    } while (option < 9);
     out.println("Have a nice day!");
   }
-  
-  static Integer read_option() {
-    out.print("run:>");  
-    String s = System.console().readLine();
-    return Integer.parseInt(s);         
+
+  private static void compute() {
+    out.print("Operation:{ +, - }:>");
+    String o = System.console().readLine();
+    out.print("First operand:>");
+    String s1 = System.console().readLine();
+    int a = Integer.parseInt(s1);
+    out.print("Second operand:>");
+    String s2 = System.console().readLine();
+    int b = Integer.parseInt(s2);
+    int result = 0;
+    if (o.equals("+")) {
+      result = myApp.operateBinary(a, b, addition);
+    } else if (o.equals("-")) {
+      result = myApp.operateBinary(a, b, subtraction);
+    } else {
+      out.printf("Operation %s not implemented.\n", o);
+      return;
+    }
+    out.printf("result = %d \n", result);
+    out.println();
   }
 
-  static void clear()   
-  {
-    out.print("\033[H\033[2J");  
-    out.flush();  
+  static Integer read_option() {
+    out.print("option:>");
+    String s = System.console().readLine();
+    return Integer.parseInt(s);
   }
-  
+
+  static void clear() {
+    //clear console
+    out.print("\033[H\033[2J");
+    out.flush();
+  }
+
 }
