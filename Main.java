af832d1afe6462f88ebc9cbf8df0ce27b3c334d6
Elucian Moise <elucian.moise@gmail.com>
2022-11-22 04:46:13 +0000

    setup repl

--- Main.java
+++ Main.java
@@ -1,5 +1,47 @@
+import static java.lang.System.out;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+ public static void print_menu() {
+    clear(); 
+    out.println("-".repeat(30));
+    out.println("Account Management!");
+    out.println("-".repeat(30));
+    out.println("0 - Clear console"); 
+    out.println("1 - Hello World");         
+    out.println("9 - Exit"); 
+    out.println("-".repeat(30));    
   }
+  
+  public static void main(String[] args) {
+    int option  = 0;
+    do {
+      switch (option) {
+        case 0:
+          print_menu(); break;
+        case 1:
+          out.println("Hello World");
+          break;
+        case 9:break;          
+        default:
+          out.format("option %d " +
+                     "not implemented.%n",option);
+          break;
+      }
+      option = read_option();       
+    } while (option < 9);      
+    out.println("Have a nice day!");
+  }
+  
+  static Integer read_option() {
+    out.print("run:>");  
+    String s = System.console().readLine();
+    return Integer.parseInt(s);         
+  }
+
+  static void clear()   
+  {
+    out.print("\033[H\033[2J");  
+    out.flush();  
+  }
+  
 }
