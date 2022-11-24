d137cb88fbe8efb3d8ee08fed54bb04b44ebe0b2
Elucian Moise <elucian.moise@gmail.com>
2022-11-24 12:27:47 +0000

    Update comments

--- Varargs.java
+++ Varargs.java
@@ -1,11 +1,12 @@
 public class Varargs {
 
-  //input & output parameter
-  public static void test(Integer ... args) {
-    for (int x: args) {
+  // Symbol ... before parameter name
+  // makes args a vararg receiver
+  public static void test(Integer... args) {
+    for (int x : args) {
       System.out.print(x);
       System.out.print(",");
-     }
-     System.out.println("");
+    }
+    System.out.println("");
   }
 }
