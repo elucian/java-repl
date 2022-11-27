9c3186f56c8a7244e2f436ae240e00bedc49da4b
Elucian Moise <elucian.moise@gmail.com>
2022-11-27 15:40:41 +0000

    fix comment

--- Filter.java
+++ Filter.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class Filter {
-    // Driver code
-    public static void test()
-    {
-        // Creating a list of Integers
-        List<Integer> list = Arrays.asList(3, 4, 6, 12, 20);
+  // Driver code
+  public static void test()
+  {
+    // Creating a list of Integers
+    List<Integer> list = Arrays.asList(3, 4, 6, 12, 20);
 
-        // elements that are divisible by 5
-        list.stream().filter (
-             num -> num % 2 == 0).forEach(System.out::println);
-    }
+    // elements that are divisible by 2
+    list.stream().filter (
+         num -> num % 2 == 0).forEach(System.out::println);
+  }
 }
